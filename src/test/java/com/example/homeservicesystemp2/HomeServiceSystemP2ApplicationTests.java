c665d7ed1723296c5b44273379053bdb7960c754
milad <baranimilad70@yahoo.com>
2024-06-23 16:25:42 +0330

    #1 - all configs added to project

---
+++ src/test/java/com/example/homeservicesystemp2/HomeServiceSystemP2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeservicesystemp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeServiceSystemP2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
